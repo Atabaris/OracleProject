994356bd38001b206c808e4cacab40ec8c184cd9
Ata <anian_376@hotmail.com>
2017-02-06 21:37:59 +0300

    Basic connection test button

--- WebApplication1/WebForm1.aspx
+++ WebApplication1/WebForm1.aspx
@@ -5,10 +5,18 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            text-align: justify;
+        }
+    </style>
 </head>
 <body>
     <form id="form1" runat="server">
-    <div>
+    <div class="auto-style1">
+    
+        <asp:Button ID="button_start" runat="server" Height="34px" OnClick="button_start_Click" Text="StartDbConnection" Width="178px" />
+        <asp:Label ID="Label1" runat="server" Height="30px" style="text-align: right; margin-left: 27px" Width="303px"></asp:Label>
     
     </div>
     </form>
--- WebApplication1/WebForm1.aspx.cs
+++ WebApplication1/WebForm1.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Oracle.DataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,23 @@ namespace WebApplication1
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void button_start_Click(object sender, EventArgs e)
+        {
+            string oradb = "Data Source=ORCL;User Id=hr;Password=hr;";
+
+            OracleConnection conn = new OracleConnection(oradb); // C#
+
+            conn.Open();
+            if (conn != null)
+            {
+                Label1.Text = "You are connected to DB!";
+            }else
+            {
+                Label1.Text = "You could not  connected to DB!";
+            }
+            conn.Dispose();
         }
     }
 }
